6e610aae30a6a2d77deaaa29cf8d5c887d8d5b67
Blytungdev <70883497+Blytungdev@users.noreply.github.com>
2022-05-11 13:40:26 +0200

    Work on parser & evaluation

--- AbstractInstruction.java
+++ AbstractInstruction.java
@@ -13,35 +13,80 @@ class MovementNode extends AbstractInstruction {
     }
 
     public void evaluate(Leona leona) {
-        leona.move(moveUnits);
+        if(moveUnits < 0) {
+            leona.moveBack(-moveUnits);
+        } else {
+            leona.moveForw(moveUnits);
+        }
     }
     
 }
 
 class RotationNode extends AbstractInstruction {
 
-    
+    private int rotationUnits;
+
+    public RotationNode(int angle) {
+        rotationUnits = angle;
+    }
+
     public void evaluate(Leona leona) {
-        // TODO Auto-generated method stub
-        
+        if(rotationUnits < 0) {
+            leona.turnRight(-rotationUnits);
+        } else {
+            leona.turnLeft(rotationUnits);
+        }
     }
 
 }
 
 class PenNode extends AbstractInstruction {
 
-    public void evaluate() {
-        // TODO Auto-generated method stub
+    private boolean changeColor; // True ==> COLOR, False ==> UP or DOWN
+    private String newColor;
+
+    private boolean newPenUp;
+
+    public PenNode(String newColor) {
+        this.newColor = newColor;
+        this.changeColor = true;
+    }
+
+    public PenNode(boolean newPenUp) {
+        this.newPenUp = newPenUp;
+        this.changeColor = false;
+    }
+
+    public void evaluate(Leona leona) {
+        if(this.changeColor) {
+            leona.changeColor(this.newColor);
+        } else {
+            if(this.newPenUp) {
+                leona.penUp();
+            } else {
+                leona.penDown();
+            }
+        }
         
     }
 
 }
 
 class RepeatNode extends AbstractInstruction {
+    private int numReps;
+    private ArrayList<AbstractInstruction> instructions;
 
-    public void evaluate() {
-        // TODO Auto-generated method stub
-        
+    public RepeatNode(int numReps, ArrayList<AbstractInstruction> instructions) {
+        this.numReps = numReps;
+        this.instructions = instructions;
+    }
+
+    public void evaluate(Leona leona) {
+        for(int i = 0; i < numReps; i++) {
+            for(AbstractInstruction instruction : instructions) {
+                instruction.evaluate(leona);
+            }
+        }
     }
 
 }
--- Parser.java
+++ Parser.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
+
 public class Parser {
     private Lexer lexer;
-    private ParseTree parseTree;
+    //private ParseTree parseTree;
 
     public ParseTree parse() throws SyntaxError{
         //start from leona expressions/instructions
         //ParseTree pt = leonaExpr();
-        parseTree = new ParseTree();
-        
-        Token t = lexer.nextToken();
+        //parseTree = new ParseTree();
+
+        ParseTree newParseTree = new ParseTree();
+        while(lexer.hasMoreTokens()) {
+            newParseTree.add(leonaExpr());
+        }
+        return newParseTree;
     }
 
-    public void leonaExpr() throws SyntaxError {
+    public AbstractInstruction leonaExpr() throws SyntaxError {
         Token t = lexer.nextToken();
         if(t.getType() == TokenType.FORW) {
             Token t1 = lexer.nextToken();
@@ -22,10 +28,11 @@ public class Parser {
                 throw new SyntaxError();
             }
             //lägg till instruktionen till nån datastruktur med andra giltiga instruktioner. 
-            parseTree.add(new MovementNode((Integer)t1.getData()));
+            return new MovementNode((Integer)t1.getData());
         }
         else if(t.getType() == TokenType.BACK) {
-            if(lexer.nextToken().getType() != TokenType.DECIMAL) {
+            Token t1 = lexer.nextToken();
+            if(t1.getType() != TokenType.DECIMAL) {
                 throw new SyntaxError();
             }
             Token t2 = lexer.nextToken();
@@ -33,10 +40,12 @@ public class Parser {
                 throw new SyntaxError();
             }
             //lägg till instruktionen till nån datastruktur med andra giltiga instruktioner. 
+            return new MovementNode(-1*(Integer)t1.getData());
 
         }
         else if(t.getType() == TokenType.LEFT) {
-            if(lexer.nextToken().getType() != TokenType.DECIMAL) {
+            Token t1 = lexer.nextToken();
+            if(t1.getType() != TokenType.DECIMAL) {
                 throw new SyntaxError();
             }
             Token t2 = lexer.nextToken();
@@ -44,9 +53,11 @@ public class Parser {
                 throw new SyntaxError();
             }
             //lägg till instruktionen till nån datastruktur med andra giltiga instruktioner. 
+            return new RotationNode((Integer)t1.getData());
         }
         else if(t.getType() == TokenType.RIGHT) {
-            if(lexer.nextToken().getType() != TokenType.DECIMAL) {
+            Token t1 = lexer.nextToken();
+            if(t1.getType() != TokenType.DECIMAL) {
                 throw new SyntaxError();
             }
             Token t2 = lexer.nextToken();
@@ -54,21 +65,25 @@ public class Parser {
                 throw new SyntaxError();
             }
             //lägg till instruktionen till nån datastruktur med andra giltiga instruktioner. 
+            return new RotationNode(-1*(Integer)t1.getData());
         }
         else if(t.getType() == TokenType.UP) {
             if(lexer.nextToken().getType() != TokenType.PERIOD) {
                 throw new SyntaxError();
             }
             //lägg till instruktionen till nån datastruktur med andra giltiga instruktioner. 
+            return new PenNode(true);
         }
         else if(t.getType() == TokenType.DOWN) {
             if(lexer.nextToken().getType() != TokenType.PERIOD) {
                 throw new SyntaxError();
             }
-            //lägg till instruktionen till nån datastruktur med andra giltiga instruktioner. 
+            //lägg till instruktionen till nån datastruktur med andra giltiga instruktioner.
+            return new PenNode(false); 
         }
         else if(t.getType() == TokenType.COLOR) {
-            if(lexer.nextToken().getType() != TokenType.HEX) {
+            Token t1 = lexer.nextToken();
+            if(t1.getType() != TokenType.HEX) {
                 throw new SyntaxError();
             }
             Token t2 = lexer.nextToken();
@@ -76,29 +91,39 @@ public class Parser {
                 throw new SyntaxError();
             }
             //lägg till instruktionen till nån datastruktur med andra giltiga instruktioner. 
+            return new PenNode((String)t1.getData());
         }
         else if(t.getType() == TokenType.REP) {
-            if(lexer.nextToken().getType() != TokenType.DECIMAL) {
+            Token t1 = lexer.nextToken();
+            if(t1.getType() != TokenType.DECIMAL) {
                 throw new SyntaxError();
             }
             Token t2 = lexer.nextToken();
             if(t2.getType() == TokenType.QUOTE) {
-                repExpr();
+                // We need an ArrayList<AbstractInstruction>
+                ArrayList<AbstractInstruction> instructions = repExpr();
+                return new RepeatNode((Integer)t1.getData(), instructions);
             }
             else {
-                leonaExpr();
+                // TODO: In case of performance issues, change to some other data structure instead of arrayList
+                ArrayList<AbstractInstruction> instructions = new ArrayList<>();
+                instructions.add(leonaExpr());
+                return new RepeatNode((Integer)t1.getData(), instructions);
             }
         }
     }
 
-    public void repExpr() throws SyntaxError {
+    public ArrayList<AbstractInstruction> repExpr() throws SyntaxError {
+        ArrayList<AbstractInstruction> instructions = new ArrayList<>();
         Token t = lexer.peekToken();
         if(t.getType() == TokenType.QUOTE) {
             //return an empty parsetree.
+            return instructions;
         }
         else {
-            while(lexer.peekToken().getType() != TokenType.QUOTE)
-            leonaExpr();
+            while(lexer.peekToken().getType() != TokenType.QUOTE) {
+                instructions.add(leonaExpr());
+            }
         }
     }
 }
